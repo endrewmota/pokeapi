63b20d130737d5b8064c18c97c83e6c9ba334aba
endrewmota <endrew.desousa@gmail.com>
2022-08-22 20:16:19 -0400

    Criando metodo GET POKEAPI

---
+++ api7daysofcode/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using RestSharp;
+
+var client = new RestClient($"https://pokeapi.co/api/v2/ability/65/");
+var request = new RestRequest("", Method.Get);
+
+var response = client.Execute(request);
+
+Console.WriteLine(response.Content);
